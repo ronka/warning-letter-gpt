f74aa3d5b81e24d62f850076a228d4da8f06e138
Ron Kantor <ronkamail@gmail.com>
2024-05-20 20:49:48 +0300

    clear app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { DirectionProvider } from "@radix-ui/react-direction";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" dir="rtl">
       <body className={inter.className}>{children}</body>
     </html>
   );
