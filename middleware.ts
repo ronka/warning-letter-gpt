c38d9ee7dab569a87da428562a69707ac42401f1
Ron Kantor <ronkamail@gmail.com>
2024-10-18 17:00:33 +0300

    protect letter routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/letter(.*)"]);
 
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
