ac63e560f5df1f52205e5ba610a170eb55a5d3bd
Ron Kantor <ronkamail@gmail.com>
2024-10-24 21:58:16 +0300

    init home page

--- app/letter/page.tsx
+++ app/letter/page.tsx
@@ -1,11 +1,8 @@
-import { Footer } from "@/components/Footer";
 import { CreateForm } from "@/components/home/CreateForm";
-import { Hero } from "@/components/home/Hero";
 
 export default function Letters() {
   return (
     <>
-      <Hero />
       <CreateForm />
     </>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import { Footer } from "@/components/Footer";
-import { CreateForm } from "@/components/home/CreateForm";
 import { Hero } from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <main className="flex-grow">
-      <h1 className="text-center text-2xl font-bold">
-        Welcome to the Home Page
-      </h1>
-      <p className="text-center mt-4">
-        Navigate to /letters to see the letters or /letter for individual letter
-        details.
-      </p>
+      <Hero />
     </main>
   );
 }
