76db305c5cd84d58c3df678c3fa47d09bcde200d
Ron Kantor <ronkamail@gmail.com>
2024-10-24 23:29:07 +0300

    fix wrong import

--- types/FormData.ts
+++ types/FormData.ts
@@ -1,4 +1,4 @@
+import { formSchema } from "@/components/letter/CreateForm";
 import { z } from "zod";
-import { formSchema } from "@/components/home/CreateForm";
 
 export type FormData = z.infer<typeof formSchema>;
