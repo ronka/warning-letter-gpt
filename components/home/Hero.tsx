77cb75eaad7f3dc4ab9a3d5a6fa0fcc69e6bd45f
Ron Kantor <ronkamail@gmail.com>
2024-11-08 16:14:55 +0200

    add a list to of pros to the hero

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -10,6 +10,14 @@ const Hero = () => {
         <p className="text-xl mb-8">
           בו לייצר מכתב התראה מהיר וקל לכל שם ומטרה שתרצה
         </p>
+
+        <ul className="text-lg mb-8">
+          <li>✅ עצירת הפצת מידע שקרי עליך או על העסק שלך</li>
+          <li>✅ שמירה על המוניטין האישי והעסקי שלך</li>
+          <li>✅ קיצור הליכים משפטיים יקרים וממושכים</li>
+          <li>✅ פתרון מהיר ומיידי למניעת פגיעה נוספת</li>
+        </ul>
+
         <Link href={"/letter"}>
           <Button className="text-lg px-8 py-6 rounded-xl hover:scale-105 transition-transform duration-200 shadow-lg hover:shadow-xl bg-primary">
             <span className="ml-2 text-2xl">✉️</span>
